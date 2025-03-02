0112a3bc5dba10fda73e41db5fcb8160221f1147
PrideRevolution <youth.priderevolution@gmail.com>
2025-03-02 23:02:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('keydown', function(event) {
+    if (event.key === "Escape") {
+        window.location.href = "https://www.google.com";  // Redirect to a safe site
+    }
+});
